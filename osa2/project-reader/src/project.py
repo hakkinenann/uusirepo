d96ec747e47da882fc26467a2ca8c3683fb4c860
hakkanka <anniina.a.hakkinen@student.jyu.fi>
2024-04-22 13:53:17 +0300

    Tehtävä 7 valmis

--- osa2/project-reader/src/project.py
+++ osa2/project-reader/src/project.py
@@ -1,17 +1,33 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, license, authors, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.license = license
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
-    def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
-
+    def _stringify_arrays(self, array):
+        if len(array) < 1: return "-"
+        palautettava = ""
+        indeksi = 0
+        for x in array:
+            if indeksi == len(array)-1:
+                palautettava = palautettava + "- " + x
+            else:
+                palautettava = palautettava + "- " + x +"\n"
+                indeksi = indeksi + 1
+        return palautettava
+    
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\nLicense: {self.license}"
+            f"\n"
+            f"\nAuthors: \n{self._stringify_arrays(self.authors)}"
+            f"\n"
+            f"\nDependencies: \n{self._stringify_arrays(self.dependencies)}"
+            f"\n"
+            f"\nDevelopment dependencies: \n{self._stringify_arrays(self.dev_dependencies)}"
         )
--- osa2/project-reader/src/project_reader.py
+++ osa2/project-reader/src/project_reader.py
@@ -16,4 +16,4 @@ class ProjectReader:
         tietorakenteen_kolmas_kerros = tietorakenteen_toka_kerros["group"]
         tietorakenteen_neljas_kerros = tietorakenteen_kolmas_kerros["dev"]
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project(tietorakenteen_toka_kerros["name"], tietorakenteen_toka_kerros["description"], tietorakenteen_toka_kerros["dependencies"], tietorakenteen_neljas_kerros["dependencies"])
+        return Project(tietorakenteen_toka_kerros["name"], tietorakenteen_toka_kerros["description"], tietorakenteen_toka_kerros["license"], tietorakenteen_toka_kerros["authors"], tietorakenteen_toka_kerros["dependencies"], tietorakenteen_neljas_kerros["dependencies"])
