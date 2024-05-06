a67c171d7452e1a296919b59654f0762d455e7af
hakkanka <anniina.a.hakkinen@student.jyu.fi>
2024-05-06 10:46:27 +0300

    Loput testit tehty ja muokattu lataa-metodia

--- osa4/maksukortti-mock/src/tests/kassapaate_test.py
+++ osa4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,18 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+    
+    def test_kortille_ladataan_saldoa_jos_positiivinen(self):
+        maksukortti_mock = Mock()
+        
+        self.kassa.lataa(maksukortti_mock, 10)
+
+        maksukortti_mock.lataa.assert_called_with(10)
+    
+    def test_kortille_ei_ladata_saldoa_jos_negatiivinen(self):
+        maksukortti_mock = Mock()
+        
+        self.kassa.lataa(maksukortti_mock, -10)
+
+        maksukortti_mock.lataa.assert_not_called()
+
